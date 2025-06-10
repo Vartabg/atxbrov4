40cd81087021c4208ef015b37a2f8fb659f32c02
Vartabg <garo.vartabed@gmail.com>
2025-06-10 03:10:13 -0500

    feat: add smooth camera glide transition on planet click - replaces hyperzoom with gentle 2s approach

--- src/components/3d/OptimizedPlanetSystem.tsx
+++ src/components/3d/OptimizedPlanetSystem.tsx
@@ -1,4 +1,6 @@
 'use client'
+import { useThree } from "@react-three/fiber"
+import { gsap } from "gsap"
 import { useState, useMemo, Suspense } from 'react'
 import { PlanetSystem } from './PlanetSystem'
 import HolographicPanel from '../ui/holographic/HolographicPanel'
@@ -15,12 +17,27 @@ function SimpleLoader() {
 }
 
 export default function OptimizedPlanetSystem() {
+ const { camera } = useThree()
   const [selectedPlanet, setSelectedPlanet] = useState<string | null>(null)
   const [panelPosition, setPanelPosition] = useState<[number, number, number]>([0, 0, 5])
 
   const handlePlanetClick = (planetId: string) => {
     const planet = defaultUniverseConfig.planets.find(p => p.id === planetId)
     if (planet) {
+     // Smooth camera glide to planet
+     const targetPosition = [
+       planet.position[0] + 8,
+       planet.position[1] + 3,
+       planet.position[2] + 10
+     ]
+     
+     gsap.to(camera.position, {
+       duration: 2,
+       x: targetPosition[0],
+       y: targetPosition[1],
+       z: targetPosition[2],
+       ease: "power2.inOut"
+     })
       setSelectedPlanet(planetId)
       setPanelPosition([planet.position[0] + 5, planet.position[1] + 2, planet.position[2]])
     }
